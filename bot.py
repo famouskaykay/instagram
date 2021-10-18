6ec87a313e90898626375a4131ec675c11a75620
famouskaykay <86409796+famouskaykay@users.noreply.github.com>
2021-10-18 17:39:52 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,79 +1,144 @@
 from selenium import webdriver
-from time import sleep
-from selenium.webdriver.common.keys import Keys
+import time
 import random
 import sys
-import os
-import re
-import firefox
-import selenium
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+class bot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Chrome("chromedriver.exe")
+        
+    def login(self):
+        print('login initiated')
+        chrome_browser = self.driver
+        chrome_browser.implicitly_wait(3)
+        chrome_browser.get('https://www.instagram.com/')
+        
+        time.sleep(5)
+        
+
+        username_input = chrome_browser.find_element_by_xpath("//input[@name='username']")
+        chrome_browser.implicitly_wait(5)
+        password_input =chrome_browser.find_element_by_xpath("//input[@name='password']")
+        chrome_browser.implicitly_wait(5)
+        username_input.clear()
+        chrome_browser.implicitly_wait(5)
+        password_input.clear()
+        chrome_browser.implicitly_wait(5)
+        
+        username_input.send_keys(self.username)
+        chrome_browser.implicitly_wait(5)
+        password_input.send_keys(self.password)
+        chrome_browser.implicitly_wait(5)
+
+        login_button = chrome_browser.find_element_by_tag_name('form')
+        login_button.submit()
+        chrome_browser.implicitly_wait(10)
+        
+        
+        print('login complete')
+        time.sleep(10)
+    
+    def searchHashtag(self,hashtag) :
+        print('serching initiated')
+        chrome_browser = self.driver
+        chrome_browser.implicitly_wait(3)
+        chrome_browser.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
+        print('searching finish')
+        time.sleep(5)
+        
+    
+    def likePhoto(self,amount):
+        chrome_browser = self.driver
+        print('opening photo')
+        chrome_browser.implicitly_wait(3)
+        chrome_browser.find_element_by_class_name('_9AhH0').click()
+        time.sleep(2)
+        
+        
+        i=1
+        while i<=amount:
+            print('likiing started')
+            try:
+                chrome_browser.find_element_by_css_selector('svg[aria-label="Like"]').click()
+            except:
+                pass
+            print('liked')
+            print(i)
+            time.sleep(2)
+            
+            print('next image')
+            try:
+                chrome_browser.find_element_by_xpath('//a[contains(text(),"Next")]').click()
+            except:
+                chrome_browser.get('https://www.instagram.com/'+self.username)
+            time.sleep(3)
+            i+=1
+        chrome_browser.get('https://www.instagram.com/'+self.username)
+            
+    
+    def securityCheck(self):
+        chrome_browser = self.driver
+        
+        
+        chrome_browser.implicitly_wait(3)
+        print('checking for two step verification')
+        try:
+            check_str=chrome_browser.find_element_by_id('verificationCodeDescription').text
+        except:
+                return False
+            
+        sample_str='Enter the code we sent'
+        if sample_str in check_str:
+            return True
+        else:
+            return False
+        
+        print('checked for two step verification ')
+    
+    def setCode(self,code):
+        chrome_browser = self.driver
+        chrome_browser.implicitly_wait(3)
+        print('seting up code given by usser')
+        securityCode=chrome_browser.find_element_by_xpath('//input[@aria-label="Security Code"]')
+        securityCode.clear()
+        securityCode.send_keys(code)
+        time.sleep(2)
+        confirm=chrome_browser.find_element_by_xpath('//button[contains(text(),"Confirm")]')
+        confirm.click()
+        print('code verification done')
+        
+    def closeBrowser(self):
+        self.driver.close()
+        
+   
+insta=bot('USERNAME','PASSWORD')
+insta.login()
+i=1
+while True:
+    
+    if insta.securityCheck():
+        if i>1:
+            print('!!!!!!opps pls enter correct code!!!!!!!!!')
+        print('pls enter code recived in your phone---')
+        code=input()
+        insta.setCode(code)
+        
+        i+=1
+    else:
+        break
+    time.sleep(15)
+
 
 username = os.environ.get("username")
 password = os.environ.get("password")
 
-def print_same_line(text):
-    sys.stdout.write('\r')
-    sys.stdout.flush()
-    sys.stdout.write(text)
-    sys.stdout.flush()
-
-class InstaBot:
-    def __init__(self, username, password):
-        self.driver = webdriver.firefox()
-        self.username = username
-        self.driver.get("https://instagram.com")
-        sleep(7)
-        self.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)
-        self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys(password)
-        self.driver.find_element_by_xpath("//button[@type='submit']").click()
-        sleep(5)
-        self.driver.find_element_by_xpath("//button[contains(text(),'Not Now')]").click()
-        sleep(5)
-        self.driver.find_element_by_xpath("//button[contains(text(),'Not Now')]").click()
-        sleep(5)
-    
-    def like_photo(self, hashtag):
-        driver = self.driver
-        driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
-        sleep(2)
-
-        # gathering photos
-        pic_hrefs = []
-        for i in range(1, 7):
-            try:
-                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                sleep(2)
-                # get tags
-                hrefs_in_view = driver.find_elements_by_tag_name('a')
-                # finding relevant hrefs
-                hrefs_in_view = [elem.get_attribute('href') for elem in hrefs_in_view if '.com/p/' in elem.get_attribute('href')]
-                # building list of unique photos
-                [pic_hrefs.append(href) for href in hrefs_in_view if href not in pic_hrefs]
-                print("Check: pic href length " + str(len(pic_hrefs)))
-            except Exception:
-                continue
-
-        # Liking photos
-        unique_photos = len(pic_hrefs)
-        for pic_href in pic_hrefs:
-            driver.get(pic_href)
-            sleep(2)
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            try:
-                sleep(random.randint(2, 4))
-                like_button = lambda: driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[1]/span[1]/button').click()
-                like_button().click()
-                for second in reversed(range(0, random.randint(18, 28))):
-                    print_same_line("#" + hashtag + ': unique photos left: ' + str(unique_photos)
-                                    + " | Sleeping " + str(second))
-                    sleep(1)
-            except Exception as e:
-                sleep(2)
-            unique_photos -= 1
+insta.searchHashtag('gainwithmchina')
+insta.likePhoto(100)
+insta.closeBrowser()       
         
         
-        
-my_bot = InstaBot("username", "password")       
-words = ['gainwithxtiandela', 'gainwithmtaaraw', 'gainwithmchina', 'chuva', 'chuvadelike']       # type in the tags without the hashtag symbol inside the apostrophes. For example, 'abstract', 'sky', 'photography' etc.
-for i in words:
-    my_bot.like_photo(i)
